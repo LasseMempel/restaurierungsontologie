5606efe11a83b2ad588f85bf4f2b3b7fc199c41f
Lasse Mempel-Länger <lassemempel@gmail.com>
2026-01-20 10:38:44 +0100

    update

--- restont.py
+++ restont.py
@@ -19,31 +19,33 @@ g.add((ontology_uri, RDF.type, OWL.Ontology))
 
 # Define classes with superclasses: CIDOC-CRM mappings here but can be internal too...
 class_mappings = {
-    "Kollektion": "E78_Curated_Holding",
-    "Objekt": "E19_Physical_Object",
+    #"Kollektion": "E78_Curated_Holding",
+    "Konservierungs_und_Restaurierungsobjekt": "E19_Physical_Object",
     "Objektmaterial": "E57_Material",
-    "Zustand": "E3_Condition_State",
+    "Physischer_Objektzustand": "E3_Condition_State",
     "Schadensphänomen": "E3_Condition_State",
-    "Methode": "E7_Activity",
-    "Agent": "E39_Actor",
-    "Restaurierungsmaterial": "E57_Material",
+    "Behandlungsmethode": "E7_Activity",
+    #"Agent": "E39_Actor",
+    "Konservierungs_und_Restaurierungsmaterial": "E57_Material",
+    "Konservierungs_und_Restaurierungswerkzeug": "E19_Physical_Object"
 }
 
 for onto_class, cidoc_class in class_mappings.items():
     g.add((ONTO[onto_class], RDF.type, OWL.Class))
+    g.add((ONTO[onto_class], RDFS.label, Literal(onto_class.replace("_", " "), lang="de")))
     g.add((ONTO[onto_class], RDFS.subClassOf, CIDOC[cidoc_class]))
 
 # Define Object Properties
 properties = {
-    "gehört_zu": (ONTO.Objekt, ONTO.Kollektion),
-    "besteht_aus": (ONTO.Objekt, ONTO.Objektmaterial),
-    "hat_Schaden": (ONTO.Objekt, ONTO.Schadensphänomen),
-    "ist_Teil_von": (ONTO.Objekt, ONTO.Objekt),
-    "durchgeführt_an": (ONTO.Methode, ONTO.Objekt),
-    "durchgeführt_durch": (ONTO.Methode, ONTO.Agent),
-    "nutzt_Werkzeug": (ONTO.Methode, ONTO.Objekt),
-    "nutzt_Material": (ONTO.Methode, ONTO.Restaurierungsmaterial),
-    "ist_Teil_von": (ONTO.Objekt, ONTO.Objekt)
+    #"gehört_zu": (ONTO.Konservierungs_und_Restaurierungsobjekt, ONTO.Kollektion),
+    "besteht_aus": (ONTO.Konservierungs_und_Restaurierungsobjekt, ONTO.Objektmaterial),
+    "hat_Schaden": (ONTO.Konservierungs_und_Restaurierungsobjekt, ONTO.Schadensphänomen),
+    "hat_Zustand": (ONTO.Konservierungs_und_Restaurierungsobjekt, ONTO.Physischer_Objektzustand),
+    "ist_Teil_von": (ONTO.Konservierungs_und_Restaurierungsobjekt, ONTO.Konservierungs_und_Restaurierungsobjekt),
+    "durchgeführt_an": (ONTO.Behandlungsmethode, ONTO.Konservierungs_und_Restaurierungsobjekt),
+    #"durchgeführt_durch": (ONTO.Behandlungsmethode, ONTO.Agent),
+    "nutzt_Werkzeug": (ONTO.Behandlungsmethode, ONTO.Konservierungs_und_Restaurierungswerkzeug),
+    "nutzt_Material": (ONTO.Behandlungsmethode, ONTO.Konservierungs_und_Restaurierungsmaterial)
 }
 
 for prop_name, (domain, range_) in properties.items():
